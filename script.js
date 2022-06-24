950bb886e05181451d8a5bd02106194d0feb9804
rickylobo626 <loboman96@gmail.com>
2022-06-24 22:16:10 +0100

    Make equal, operator and number buttons work

--- script.js
+++ script.js
@@ -1,31 +1,103 @@
 'use strict';
 
-const operationDisplay = document.getElementById('operation');
-const currentDisplay = document.getElementById('current');
 const clearBtn = document.getElementById('clear');
 const clearEntryBtn = document.getElementById('clear-entry');
 const equalBtn = document.getElementById('equal');
 const numberBtns = document.querySelectorAll('[data-number]');
 const operatorBtns = document.querySelectorAll('[data-operator]');
 const pointBtn = document.querySelector('[data-point]');
+let operationDisplay = document.getElementById('operation');
+let currentDisplay = document.getElementById('current');
+let currentNum, previousNum, currentOperator, equalWasClicked;
+reset();
 
 numberBtns.forEach(button => {
-  button.addEventListener('click', () => {
-
-  });
+  button.addEventListener('click', () => enterNumber(button.textContent));
 });
 
 operatorBtns.forEach(button => {
-  button.addEventListener('click', () => {
-
-  });
+  button.addEventListener('click', () => enterOperator(button.textContent));
 });
 
-equalBtn.addEventListener('click', () => {
+equalBtn.addEventListener('click', evaluate);
+
+clearBtn.addEventListener('click', () => {
   
 });
 
+function enterNumber(num) {
+  if (equalWasClicked) {
+    reset();
+    currentDisplay.textContent += num;
+  }
+  //if display is only a 0 or if current num is empty
+  if (currentDisplay.textContent == '0' || currentNum == '') {
+    currentDisplay.textContent = '';
+    currentDisplay.textContent += num;
+  } else {
+    currentDisplay.textContent += num;
+  }
 
+  
+  
+    
+  // Store number in the currentNum variable 
+  currentNum = currentDisplay.textContent;
+  console.log({previousNum});
+  console.log({currentNum});
+}
+
+function enterOperator(op) {
+  
+
+  // If two numbers are stored, operate
+  if (previousNum != '' && currentNum != '') {
+    currentNum = operate(currentOperator, previousNum, currentNum);
+    currentDisplay.textContent = currentNum;
+    currentOperator = null;
+    previousNum = '';
+  }
+
+  // Store currentNum on previousNum and empty currentNum for the next value
+  previousNum = currentNum;
+  currentNum = '';
+  
+  // store operator
+  currentOperator = op;
+
+  equalWasClicked = false;
+
+  console.log({previousNum});
+  console.log({currentNum});
+  console.log({currentOperator});
+}
+
+function evaluate() {
+  // If two numbers are stored, operate
+  if (previousNum != '' && currentNum != '') {
+    currentNum = operate(currentOperator, previousNum, currentNum);
+    currentDisplay.textContent = currentNum;
+    currentOperator = null;
+    previousNum = '';
+  } 
+
+  // Store currentNum on previousNum and empty currentNum for the next value
+  previousNum = currentNum;
+  currentNum = '';
+  
+  equalWasClicked = true;
+
+  console.log({previousNum});
+  console.log({currentNum});
+  console.log({currentOperator});
+}
+
+function reset() {
+  currentNum = '';
+  previousNum = '';
+  currentOperator = null;
+  equalWasClicked = false;
+}
 
 function operate(operator, a, b) {
   a = Number(a);
